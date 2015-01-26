7fc5d6d9b470f6912995e04d63b34d925f89c81e
davidh- <davidhooper4@berkeley.edu>
2015-01-26 14:56:51 -0800

    added lec3 exercise for recursive call for IntList

--- lec3/exercises/IntList.java
+++ lec3/exercises/IntList.java
@@ -34,8 +34,9 @@ public class IntList {
       * Returns ith item of this IntList. For 
       * simplicity, assume the item exists. */
     public int get(int i) {
-        return 0;
-        /** your code here */
+        if (i == 0)
+            return this.head;
+        return tail.get(i - 1);
     }
 
     public String toString() {
