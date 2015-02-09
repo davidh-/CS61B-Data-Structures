e2e37c8c3e4f8729d5e6f6406c7e48c6b9f29de2
davidh- <davidhooper4@berkeley.edu>
2015-02-09 11:07:30 -0800

    Working on Board.java

--- proj0/Board.java
+++ proj0/Board.java
@@ -3,9 +3,54 @@ public class Board {
 	 *  @author David Dominguez Hooper
 	 *  [Do not modify this file.]
 	 */
-	
+    private static boolean[][] pieces;
+
+    private boolean emptyBoard;
+    private Piece gamePieces[][];
+
+
+    private static void drawBoard(int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if ((i + j) % 2 == 0) StdDrawPlus.setPenColor(StdDrawPlus.GRAY);
+                else                  StdDrawPlus.setPenColor(StdDrawPlus.RED);
+                StdDrawPlus.filledSquare(i + .5, j + .5, .5);
+                StdDrawPlus.setPenColor(StdDrawPlus.WHITE);
+                if (pieces[i][j]) {
+                    StdDrawPlus.picture(i + .5, j + .5, "img/bomb-fire-crowned.png", 1, 1);
+                }
+            }
+        }
+    }
+
+    private void startBoardGame() {
+        int N = 8;
+        StdDrawPlus.setXscale(0, N);
+        StdDrawPlus.setYscale(0, N);
+        pieces = new boolean[N][N];
+
+        /** Monitors for mouse presses. Wherever the mouse is pressed,
+            a new piece appears. */
+        while(true) {
+            drawBoard(N);
+            if (StdDrawPlus.mousePressed()) {
+                double x = StdDrawPlus.mouseX();
+                double y = StdDrawPlus.mouseY();
+                pieces[(int) x][(int) y] = true;
+            }            
+            StdDrawPlus.show(100);
+        }
+    }
+
+
+
 	public Board(boolean shouldBeEmpty) {
-		
+		this.emptyBoard = shouldBeEmpty;
+		this.gamePieces = new Piece[8][8];
+		// if(shouldBeEmpty == false)
+
+
+
 	}
 
 	public Piece pieceAt(int x, int y) {
@@ -44,7 +89,9 @@ public class Board {
 		return null;
 	}
 
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		Board gameBoard = new Board(false);
+		gameBoard.startBoardGame();
 	}
 }
