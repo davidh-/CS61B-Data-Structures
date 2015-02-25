b244ef4da59a8f7c66d3cfefe0f13c2c3c2b8a3b
davidh- <davidhooper4@berkeley.edu>
2015-02-24 21:32:49 -0800

    Finised Plip.java and TestPlip.java

--- lab6/creatures/Plip.java
+++ lab6/creatures/Plip.java
@@ -18,6 +18,9 @@ public class Plip extends Creature {
     private int g;
     /** blue color. */
     private int b;
+    /** probability of taking a move when ample space available and clorus around. */
+    private double moveProbability = 0.5;
+
 
     /** creates plip with energy equal to E. */
     public Plip(double e) {
@@ -41,7 +44,9 @@ public class Plip extends Creature {
      *  that you get this exactly correct.
      */
     public Color color() {
-        g = 63;
+        r = 99;
+        b = 76;
+        g = (int) Math.round(63 + (192 * (this.energy/2)));
         return color(r, g, b);
     }
 
@@ -54,11 +59,18 @@ public class Plip extends Creature {
      *  private static final variable. This is not required for this lab.
      */
     public void move() {
+        this.energy -= 0.15;
     }
 
 
     /** Plips gain 0.2 energy when staying due to photosynthesis. */
     public void stay() {
+        if (this.energy + 0.2 > 2) {
+            this.energy = 2;
+        }
+        else {
+            this.energy += 0.2; 
+        }
     }
 
     /** Plips and their offspring each get 50% of the energy, with none
@@ -66,7 +78,8 @@ public class Plip extends Creature {
      *  Plip.
      */
     public Plip replicate() {
-        return this;
+        this.energy *= 0.5;
+        return new Plip(this.energy);
     }
 
     /** Plips take exactly the following actions based on NEIGHBORS:
@@ -80,6 +93,21 @@ public class Plip extends Creature {
      *  for an example to follow.
      */
     public Action chooseAction(Map<Direction, Occupant> neighbors) {
-        return new Action(Action.ActionType.STAY);
+        List<Direction> empties = getNeighborsOfType(neighbors, "empty");
+        List<Direction> clorusNeighbors = getNeighborsOfType(neighbors, "clorus");
+        if (empties.size() == 0) {
+            return new Action(Action.ActionType.STAY);
+        }
+        else if (this.energy >= 1) {
+            Direction moveDir = HugLifeUtils.randomEntry(empties);
+            return new Action(Action.ActionType.REPLICATE, moveDir);
+        }
+        else if (clorusNeighbors.size() > 0 && HugLifeUtils.random() < moveProbability) {
+            Direction moveDir = HugLifeUtils.randomEntry(empties);
+            return new Action(Action.ActionType.MOVE, moveDir);
+        }
+        else {
+            return new Action(Action.ActionType.STAY);
+        }
     }
 }
--- lab6/creatures/TestPlip.java
+++ lab6/creatures/TestPlip.java
@@ -9,6 +9,8 @@ import huglife.Occupant;
 import huglife.Impassible;
 import huglife.Empty;
 
+import java.util.List;
+
 /** Tests the plip class   
  *  @authr FIXME
  */
@@ -32,26 +34,54 @@ public class TestPlip {
 
     @Test
     public void testReplicate() {
-
+        Plip p = new Plip(2);
+        Plip offspring = p.replicate();
+        assertNotSame(p, offspring);
+        assertEquals(1.0, p.energy(), 0.01);
+        assertEquals(1.0, offspring.energy(), 0.01);
     }
 
-    //@Test
+    @Test
     public void testChoose() {
+        // This tests if there are no empty spaces, the Plip should stay
         Plip p = new Plip(1.2);
         HashMap<Direction, Occupant> surrounded = new HashMap<Direction, Occupant>();
         surrounded.put(Direction.TOP, new Impassible());
         surrounded.put(Direction.BOTTOM, new Impassible());
         surrounded.put(Direction.LEFT, new Impassible());
-        surrounded.put(Direction.RIGHT, new Impassible());
-
-        //You can create new empties with new Empty();
-        //Despite what the spec says, you cannot test for Cloruses nearby yet.
-        //Sorry!  
-
+        surrounded.put(Direction.RIGHT, new Impassible()); 
         Action actual = p.chooseAction(surrounded);
         Action expected = new Action(Action.ActionType.STAY);
-
         assertEquals(expected, actual);
+
+
+        //You can create new empties with new Empty();
+        // This tests if it replicates to another empty space if energy > 1.0.
+        Plip replicateTest = new Plip(1.2);
+        HashMap<Direction, Occupant> emptyAllAround = new HashMap<Direction, Occupant>();
+        emptyAllAround.put(Direction.TOP, new Empty());
+        emptyAllAround.put(Direction.BOTTOM, new Impassible());
+        emptyAllAround.put(Direction.LEFT, new Impassible());
+        emptyAllAround.put(Direction.RIGHT, new Impassible());
+        List<Direction> empties = replicateTest.getNeighborsOfType(emptyAllAround, "empty");
+        Direction moveDir = empties.get(0);
+        Action actualRepTest = replicateTest.chooseAction(emptyAllAround);
+        Action expectedRepTest = new Action(Action.ActionType.REPLICATE, moveDir);
+        assertEquals(expectedRepTest, actualRepTest);
+
+        //Despite what the spec says, you cannot test for Cloruses nearby yet.
+        //Sorry! 
+
+        // This tests the else condition, Plip should stay otherwise
+        Plip otherwiseTest = new Plip(0.3);
+        HashMap<Direction, Occupant> emptyButTwo = new HashMap<Direction, Occupant>();
+        emptyButTwo.put(Direction.TOP, new Impassible());
+        emptyButTwo.put(Direction.BOTTOM, new Empty());
+        emptyButTwo.put(Direction.LEFT, new Impassible());
+        emptyButTwo.put(Direction.RIGHT, new Impassible());
+        Action actualOtherwiseTest = otherwiseTest.chooseAction(emptyButTwo);
+        Action expectedOtherwiseTest = new Action(Action.ActionType.STAY);
+        assertEquals(expectedOtherwiseTest, actualOtherwiseTest);
     }
 
     public static void main(String[] args) {
