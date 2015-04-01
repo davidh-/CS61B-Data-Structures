b319f02efd342853d15a1f3449999e1645eaf4aa
davidh- <davidhooper4@berkeley.edu>
2015-03-31 22:23:21 -0700

    changed Bin15.java again

--- hw7/Bin15.java
+++ hw7/Bin15.java
@@ -51,7 +51,7 @@ public class Bin15 {
     in the method followUpAnswer(). 
     */
     public static final int followUpAnswer() {
-        return 2147483647; // YOUR CODE HERE. THIS MAY OR MAY NOT BE CORRECT.
+        return 16; // YOUR CODE HERE. THIS MAY OR MAY NOT BE CORRECT.
     }
     
     public static void main(String[] args) {
